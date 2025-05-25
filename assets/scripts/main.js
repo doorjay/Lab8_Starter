90716704676896ab3e7c2bd0777494abc56c9b65
doorjay <dorjekmp@gmail.com>
2025-05-24 19:35:50 -0700

    I think I have everything, but the install thing is not coming up

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,6 +45,23 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if ('serviceWorker' in navigator) 
+    {
+    window.addEventListener('load', () => 
+    {
+      navigator.serviceWorker.register('./sw.js').then(regestration => 
+      {
+        console.log('Service Worker registered with scope: ', regestration.scope);
+      }).catch(error => 
+      {
+        console.error('Service Worker regestration failed: ', error);
+      });
+    });
+  }
+  else 
+  {
+    // something here... maybe
+  }
   // B2. TODO - Listen for the 'load' event on the window object.
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
